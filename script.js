b3f79edb999316de8a55bb5caba6cb61528caf25
rohannj1993 <rohannj1993@gmail.com>
2021-06-01 15:04:09 -0400

    made rows and colmuns and got api key

---
+++ script.js
@@ -0,0 +1,5 @@
+// Need to use weather api to get daily weather reports with UV results and wind speed as well as humidty.
+var apiCall = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=48da628734e9fec404e0b749f53c4fc7"
+
+var searchInput = document.querySelector("#search")
+var searchInput = document.querySelector("#form")
