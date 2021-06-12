113ef5520cbbca93c1cf75ca5e7ac375386ab822
rohannj1993 <rohannj1993@gmail.com>
2021-06-12 11:04:17 -0400

    added api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Need to use weather api to get daily weather reports with UV results and wind speed as well as humidty.
-var apiCall = "https://api.openweathermap.org/data/2.5/onecall?lat=40&lon=74&exclude={part}&appid=48da628734e9fec404e0b749f53c4fc7"
+var apiCall = "https://api.openweathermap.org/data/2.5/onecall?lat=40&lon=-74&exclude={part}&appid=48da628734e9fec404e0b749f53c4fc7"
 
 var searchInput = document.querySelector("#search")
 var searchForm = document.querySelector("#form")
@@ -9,20 +9,33 @@ var searchButton = document.querySelector("#btn")
 
 
 function searchCity() {
-console.log("Hello")
+  console.log("Hello")
 
 
 
-fetch(apiCall).then(function(response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    }
-    else {
-      alert("There was a problem with your request!");
-    }
-  });
+  fetch(apiCall)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        return response.json()
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
-  searchButton.addEventListener("click", searchCity) 
+function getLocation() {
+  var geoCity = document.getElementById("search").value;
+  console.log(geoCity)
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${geoCity}&limit=1&appid=48da628734e9fec404e0b749f53c4fc7`)
+  .then(function(response){
+    return response.json();
+  }
+  )
+  .then(function(data){
+    console.log(data)
+  })
+}
+
+
+searchButton.addEventListener("click", getLocation)
