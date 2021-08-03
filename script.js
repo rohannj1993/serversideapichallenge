5cf8660f08faf2ec173b2a93da5d017424417524
rohannj1993 <rohannj1993@gmail.com>
2021-08-03 17:57:27 -0400

    rendered additional html weather elements

--- script.js
+++ script.js
@@ -17,6 +17,32 @@ function searchCity(lat,lon) {
     })
     .then(function (data) {
       console.log(data);
+      var current = document.querySelector(".current")
+      var temp = "temperature:"+data.current.temp
+      var humidity = "humidity:"+data.current.humidity
+      var uvi = "uvi:"+data.current.uvi
+      var windSpeed = "windSpeed:"+data.current.wind_speed
+      current.append(temp,humidity,uvi,windSpeed)
+      var fiveDay = document.querySelector(".five-day")
+      var tempday1 = "temperature:"+data.daily[0].temp.day
+      var windday1 ="windSpeed:"+data.daily[0].wind_speed
+      var humidity1 ="humidity:"+data.daily[0].humidity
+      var tempday2 = "temperature:"+data.daily[1].temp.day
+      var windday2 = "windSpeed:"+data.daily[1].temp.day
+      var humidity2 ="humidity:"+data.daily[1].humidity
+      var tempday3 = "temperature:"+data.daily[2].temp.day
+      var windday3 ="windSpeed:"+data.daily[2].wind_speed
+      var humidity3 ="humidity:"+data.daily[2].humidity
+      var tempday4 = "temperature:"+data.daily[3].temp.day
+      var windday4 ="windSpeed:"+data.daily[3].wind_speed
+      var humidtiy4 ="humidity:"+data.daily[3].humidity
+      var tempday5 = "temperature:"+data.daily[4].temp.day
+      var windday5 ="windSpeed:"+data.daily[4].wind_speed
+      var humidtiy5 ="humidity:"+data.daily[4].humidity
+      
+
+      
+      fiveDay.append(tempday1,windday1,humidity1,tempday2,windday2,humidtiy2,tempday3,windday3,humidity3,tempday4,windday4,humidtiy4,tempday5,windday5,humidtiy5)
     });
 }
 function getLocation() {
@@ -38,3 +64,7 @@ function getLocation() {
 
 
 searchButton.addEventListener("click", getLocation)
+
+function getUV() {
+  
+}
