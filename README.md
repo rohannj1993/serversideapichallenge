6ea97f2cf72eecbc691e8f25b60924ea2972796c
rohannj1993 <rohannj1993@gmail.com>
2021-08-11 16:41:08 -0400

    updated readme

--- README.md
+++ README.md
@@ -6,17 +6,18 @@ Successes-
 Was able to create a search bar where users can enter cities
 Webpage has stored data on what cities were searched for in the past by user
 Used Openweather Api to retrieve information
-If user goes on console page they will be able to find temperature results for various times for city that was searched
+Can retrive weather results for city entered
+Shows image icon of weather of city entered
+
 
 In progress-
-Need to have results appear on page rather than just console
-Need to display UV information and humidty
+Need  to have previous search results visible on page, in addition to appearing on search bar
+Need to have icons and individual cards of five day forecast of city that was entered
+
 
 
 I have also provided screen shots of the webpage in various stages of use.
 
-![screenshot](assets/Screenshot50.png)
-![screenshot](assets/Screenshot51.png)
-![screenshot](assets/Screenshot52.png)
-![screenshot](assets/Screenshot53.png)
-
+![screenshot](assets/Screenshot120.png)
+![screenshot](assets/Screenshot121.png)
+![screenshot](assets/Screenshot122.png)
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var searchButton = document.querySelector("#btn")
 
 
 
-function searchCity(lat,lon) {
+function searchCity(lat, lon) {
   fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=48da628734e9fec404e0b749f53c4fc7`)
     .then(function (response) {
       // request was successful
@@ -18,53 +18,64 @@ function searchCity(lat,lon) {
     .then(function (data) {
       console.log(data);
       var current = document.querySelector(".current")
-      var temp = "temperature:"+data.current.temp
-      var humidity = "humidity:"+data.current.humidity
-      var uvi = "uvi:"+data.current.uvi
-      var windSpeed = "windSpeed:"+data.current.wind_speed
-      current.append(temp,humidity,uvi,windSpeed)
-      var fiveDay = document.querySelector(".five-day")
-      var tempday1 = "temperature:"+data.daily[0].temp.day
-      var windday1 ="windSpeed:"+data.daily[0].wind_speed
-      var humidity1 ="humidity:"+data.daily[0].humidity
-      var tempday2 = "temperature:"+data.daily[1].temp.day
-      var windday2 = "windSpeed:"+data.daily[1].temp.day
-      var humidity2 ="humidity:"+data.daily[1].humidity
-      var tempday3 = "temperature:"+data.daily[2].temp.day
-      var windday3 ="windSpeed:"+data.daily[2].wind_speed
-      var humidity3 ="humidity:"+data.daily[2].humidity
-      var tempday4 = "temperature:"+data.daily[3].temp.day
-      var windday4 ="windSpeed:"+data.daily[3].wind_speed
-      var humidtiy4 ="humidity:"+data.daily[3].humidity
-      var tempday5 = "temperature:"+data.daily[4].temp.day
-      var windday5 ="windSpeed:"+data.daily[4].wind_speed
-      var humidtiy5 ="humidity:"+data.daily[4].humidity
-      
+      var temp = document.createElement("p")
+       temp = "temperature:" + data.current.temp
+      var humidity = document.createElement("p")
+       humidity = "humidity:" + data.current.humidity
+       var uvi = document.createElement("p")
+       uvi = "uvi:" + data.current.uvi
+      var windSpeed = "windSpeed:" + data.current.wind_speed
+      var iconImg = document.createElement("img")
+      iconImg.setAttribute('src',
+        `http://openweathermap.org/img/w/` + data.current.weather[0].icon + `.png`)
+      current.append(temp, humidity, uvi, windSpeed, iconImg)
+      for (var i = 0; i < 5; i++) {
+        var fiveDay = document.querySelector(".five-day")
+        // document.createElement
+        var tempday1 = "temperature:" + data.daily[i].temp.day
+        var windday1 = "windSpeed:" + data.daily[i].wind_speed
+        var humidity1 = "humidity:" + data.daily[i].humidity
+        fiveDay.append(tempday1, windday1, humidity1)
+      }
 
-      
-      fiveDay.append(tempday1,windday1,humidity1,tempday2,windday2,humidtiy2,tempday3,windday3,humidity3,tempday4,windday4,humidtiy4,tempday5,windday5,humidtiy5)
+      // var tempday2 = "temperature:" + data.daily[1].temp.day
+      // var windday2 = "windSpeed:" + data.daily[1].temp.day
+      // var humidity2 = "humidity:" + data.daily[1].humidity
+      // var tempday3 = "temperature:" + data.daily[2].temp.day
+      // var windday3 = "windSpeed:" + data.daily[2].wind_speed
+      // var humidity3 = "humidity:" + data.daily[2].humidity
+      // var tempday4 = "temperature:" + data.daily[3].temp.day
+      // var windday4 = "windSpeed:" + data.daily[3].wind_speed
+      // var humidtiy4 = "humidity:" + data.daily[3].humidity
+      // var tempday5 = "temperature:" + data.daily[4].temp.day
+      // var windday5 = "windSpeed:" + data.daily[4].wind_speed
+      // var humidtiy5 = "humidity:" + data.daily[4].humidity
+
+
+
+      // fiveDay.append(tempday1, windday1, humidity1, tempday2, windday2, humidity2, tempday3, windday3, humidity3, tempday4, windday4, humidtiy4, tempday5, windday5, humidtiy5)
     });
 }
 function getLocation() {
   var geoCity = document.getElementById("search").value;
   console.log(geoCity)
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${geoCity}&limit=1&appid=48da628734e9fec404e0b749f53c4fc7`)
-  .then(function(response){
-    return response.json();
-  }
-  )
-  .then(function(data){
-    console.log(data)
-    var lat = data[0].lat ;
-    var lon= data[0].lon ;
-    console.log(lat,lon)
-    searchCity(lat,lon)
-  })
+    .then(function (response) {
+      return response.json();
+    }
+    )
+    .then(function (data) {
+      console.log(data)
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      console.log(lat, lon)
+      searchCity(lat, lon)
+    })
 }
 
 
 searchButton.addEventListener("click", getLocation)
 
 function getUV() {
-  
+
 }
