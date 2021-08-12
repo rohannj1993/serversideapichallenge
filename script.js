bf17f8b2df5bbc1687a11cb9875321949de0fffe
rohannj1993 <rohannj1993@gmail.com>
2021-08-12 17:27:49 -0400

    updated fetch url

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function searchCity(lat, lon) {
 function getLocation() {
   var geoCity = document.getElementById("search").value;
   console.log(geoCity)
-  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${geoCity}&limit=1&appid=48da628734e9fec404e0b749f53c4fc7`)
+  fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${geoCity}&limit=1&appid=48da628734e9fec404e0b749f53c4fc7`)
     .then(function (response) {
       return response.json();
     }
